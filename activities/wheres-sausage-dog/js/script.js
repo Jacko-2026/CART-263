45ef2a03ef9df214e49580bdf8ddc56e3ec13515
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2022-01-15 13:46:04 -0500

    03

--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const NUM_ANIMALS_IMAGES = 10;
+const NUM_ANIMAL_IMAGES = 10;
 const NUM_ANIMALS = 100;
 
 let animalImages = [];
 let animals = [];
 
 function preload() {
-  for (let i = 0; i < NUM_ANIMALS_IMAGES; i++) {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
     let animalImage = loadImage('assets/images/animal${i}.png')
     animalImages.push(animalImage);
   }
