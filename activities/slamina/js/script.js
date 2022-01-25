24bf36bbb280dd30138cd93bce77c24582959b1e
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2022-01-25 15:34:10 -0500

    Timer & Sates
    Added a timer and states

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -137,19 +137,27 @@ const animals =  [
       "zebra"
     ];
 
+    // States
+    let state = 'title';
+    let levelText = ``;
+
     let currentAnimal = '';
     let currentAnswer = '';
+
+    // Timer
+    let iText = ``;
+    let timerValue = 10;
 
     // Sounds
     let rightAnswer;
     let wrongAnswer;
 
-    function preload() {
-    // Sounds
-      soundFormats('wav');
-      rightAnswer = loadSound('assets/sounds/ding');
-      wrongAnswer = loadSound('assets/sounds/buzz');
-    }
+function preload() {
+  // Sounds
+    soundFormats('wav');
+    rightAnswer = loadSound('assets/sounds/ding');
+    wrongAnswer = loadSound('assets/sounds/buzz');
+  }
 
 function setup() {
   createCanvas(windowHeight, windowHeight);
@@ -166,6 +174,13 @@ function setup() {
     textStyle(BOLD);
     textAlign(CENTER, CENTER);
   }
+
+  // Create Timer
+  push();
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  setInterval(timeIt, 1000);
+  pop();
 }
 
 function draw() {
@@ -178,9 +193,126 @@ function draw() {
     fill(255, 0, 0);
   }
   text(currentAnswer, width/2, height/2);
+
+  // Timer
+  push();
+  textSize(55);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text(iText, 620, 55);
+  pop();
+  displayTimer();
+
+  // Screens/States
+if (state === 'title') {
+title();
+}
+else if (state === 'level1') {
+level1();
+}
+else if (state === 'levelGameOver') {
+levelGameOver();
+}
+}
+
+// Levels +Title Screen & Victory Screens
+function title() {
+  push();
+  fill(255, 255, 0);
+  square(0, 0, 10000)
+  pop();
+
+  push();
+  textSize(100);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text('START', width / 2, height / 2);
+  pop();
+
+  push();
+  textSize(25);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text('[PRESS "ENTER"]', width / 2, 725);
+  pop();
+
+  if ((state === 'title') && (keyIsDown(13))) {
+    state = 'level1';
+  }
+  timerValue = 59;
+}
+function level1() {
+  if (timerValue < 55) {
+  levelText = ``;
+  }
+  if (timerValue > 55) {
+  levelText = `[GUESS THE ANIMAL]`;
+  }
+
+  push();
+  textSize(54);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text(levelText, width / 2, height / 2);
+  pop();
+
+  if (timerValue == 0) {
+  state = 'levelGameOver';
+  }
+}
+function levelGameOver() {
+  push();
+  textSize(85);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text('GAME OVER!', width / 2, height / 2);
+  pop();
+
+  push();
+  textSize(65);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text('[PRESS "E" TO CONTINUE]', width / 2, 725);
+  pop();
+
+  if (keyIsDown(69)) {
+    state = 'title';
+    timerValue = 59;
+  }
+}
+
+//Timer
+function displayTimer() {
+  if (timerValue >= 10) {
+    textSize(30);
+    fill(28, 0, 189);
+    textAlign(CENTER, CENTER)
+    text("0:" + timerValue, 775 / 2, 50 / 2);
+  }
+  if (timerValue < 10) {
+    textSize(30);
+    fill(28, 0, 189);
+    textAlign(CENTER, CENTER)
+    text('0:0' + timerValue, 775 / 2, 50 / 2);
+  }
+  if (timerValue == 0) {
+    textSize(30);
+    fill(28, 0, 189);
+    textAlign(CENTER, CENTER)
+    text('Level Over', 725 / 2, 50 / 2 + 20);
+  }
+}
+
+function timeIt() {
+  if (timerValue > 0) {
+    timerValue--;
+  }
 }
 
 function mousePressed() {
+// Reset Timer
+  timerValue = 54;
+
   if (currentAnswer === currentAnimal) {
     rightAnswer.play();
   }
