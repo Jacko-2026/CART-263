47316dc7e702f9dab750f532dacba7ca3eadddb6
Jacko-2026 <taddeojack@gmail.com>
2022-02-15 14:38:04 -0500

    Created a timer

--- experiments/juggle-garden copy/js/script.js
+++ experiments/juggle-garden copy/js/script.js
@@ -7,6 +7,9 @@ let paddle;
 let balls = [];
 let numBalls = 3;
 
+// Timer
+let iText = ``;
+let timerValue = 10;
 
 function setup() {
   createCanvas(800,800);
@@ -19,6 +22,13 @@ function setup() {
     let ball = new Ball(x,y);
     balls.push(ball);
   }
+
+  // Create Timer
+  push();
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  setInterval(timeIt, 1000);
+  pop();
 }
 
 function draw() {
@@ -35,5 +45,41 @@ function draw() {
       ball.bounce(paddle);
       ball.display();
     }
+  }
+
+  // Timer
+  push();
+  textSize(55);
+  fill(28, 0, 189);
+  textAlign(CENTER, CENTER);
+  text(iText, 620, 55);
+  pop();
+  displayTimer();
+}
+
+//Timer
+function displayTimer() {
+  if (timerValue >= 10) {
+    textSize(30);
+    fill(255, 255, 255);
+    textAlign(CENTER, CENTER)
+    text("0:" + timerValue, 400, 25);
+  }
+  if (timerValue < 10) {
+    textSize(30);
+    fill(255, 255, 255);
+    textAlign(CENTER, CENTER)
+    text('0:0' + timerValue, 400, 25);
+  }
+  if (timerValue == 0) {
+    textSize(30);
+    fill(255, 255, 255);
+    text('Level Over', 400, 60);
+  }
+}
+
+function timeIt() {
+  if (timerValue > 0) {
+    timerValue--;
   }
 }
