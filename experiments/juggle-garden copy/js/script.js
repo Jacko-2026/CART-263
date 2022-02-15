6611943d8753e3fb7e01e723b5f03e5239c40d3e
Jacko-2026 <taddeojack@gmail.com>
2022-02-15 13:47:14 -0500

    Reset code to basics
    Basics = how it was constructed in the original video

--- experiments/juggle-garden copy/js/script.js
+++ experiments/juggle-garden copy/js/script.js
@@ -7,10 +7,6 @@ let paddle;
 let balls = [];
 let numBalls = 10;
 
-let superBalls = [];
-let numSuperBalls = 1;
-
-let state = 'simulation';
 
 function setup() {
   createCanvas(800,800);
@@ -23,27 +19,10 @@ function setup() {
     let ball = new Ball(x,y);
     balls.push(ball);
   }
-
-  for (let i = 0; i < numSuperBalls; i++) {
-    let x = random(0,width);
-    let y = random(-400,-100);
-    let superBall = new SuperBall(x,y);
-    superBalls.push(superBall);
-  }
 }
 
 function draw() {
   background(0);
-
-  if (state === 'simulation') {
-    simulation();
-  }
-  else if (state === 'badEnding') {
-    badEnding();
-  }
-  else if (state === 'goodEnding') {
-    goodEnding();
-  }
 
   paddle.move();
   paddle.display();
@@ -55,16 +34,6 @@ function draw() {
       ball.move();
       ball.bounce(paddle);
       ball.display();
-    }
-  }
-
-  for (let i = 0; i < superBalls.length; i++) {
-    let superBall = superBalls[i];
-    if (superBall.active) {
-      superBall.gravity(gravityForce);
-      superBall.move();
-      superBall.bounce(paddle);
-      superBall.display();
     }
   }
 }
@@ -79,28 +48,4 @@ function simulation(){
       state = 'goodEnding'
     }
   }
-}
-
-function badEnding() {
-  push();
-  textSize(100);
-  fill(200,100,100);
-  textAlign(CENTER,CENTER);
-  text('GAME OVER',width/2,height/2);
-  pop();
-}
-
-function goodEnding() {
-  push();
-  textSize(100);
-  fill(200,100,100);
-  textAlign(CENTER,CENTER);
-  text('CONGRATS!',width/2,height/2);
-  pop();
-}
-
-
-function mouseClicked() {
-  let ball = new Ball(mouseX,mouseY);
-  balls.push(ball);
 }
