fd75f0166db2384086b0d6f0114e3cd5788c4b23
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2022-04-12 15:15:27 -0400

    Item/Clients/Price
    I setup item and client images, as well as set a text for the current price and a detection for if the item is stolen or not

--- projects/project2/version02/js/script.js
+++ projects/project2/version02/js/script.js
@@ -7,17 +7,47 @@ The aim of the game is to buy, sell, and trade items that come to your shop by i
 
 "use strict";
 
+let clientImage = document.getElementById(`client-image`);
+let clientImages = [
+  `assets/images/clients/Tadloanian-01.png`,
+  `assets/images/clients/Android-01.png`,
+  `assets/images/clients/Android-02.png`,
+  `assets/images/clients/Drogo-01.png`,
+  `assets/images/clients/Drogo-02.png`,
+  `assets/images/clients/Drogo-03.png`,
+  `assets/images/clients/Ghost-01.png`,
+  `assets/images/clients/Ghost-02.png`,
+  `assets/images/clients/GUF-01.png`,
+  `assets/images/clients/GUF-02.png`,
+  `assets/images/clients/Java-01.png`,
+  `assets/images/clients/Java-02.png`,
+  `assets/images/clients/Mando-01.png`,
+  `assets/images/clients/Mando-02.png`,
+  `assets/images/clients/Mando-03.png`
+];
+clientImage.src = random(clientImages);
 let dialogue = [
   `Give me 100$.`,
   `Give me 200$.`,
   `Give me 300$.`
 ];
 
-let clientImage = document.getElementById(`client-image`);
-let clientImages = [
-  `assets/images/Item-01.png`
+let itemImage = document.getElementById(`item-image`);
+let itemImages = [
+  `assets/images/items/Item-01.png`,
+  `assets/images/items/Item-02.png`,
+  `assets/images/items/Item-03.png`,
+  `assets/images/items/Item-04.png`,
+  `assets/images/items/Item-05.png`,
+  `assets/images/items/Item-06.png`,
+  `assets/images/items/Item-07.png`,
+  `assets/images/items/Item-08.png`,
+  `assets/images/items/Item-09.png`,
+  `assets/images/items/Item-10.png`,
+  `assets/images/items/Item-11.png`,
+  `assets/images/items/Item-12.png`
 ];
-clientImage.src = random(clientImages);
+itemImage.src = random(itemImages);
 
 let customerJoke = [
   `Whats the best thing about the Swiss?
@@ -49,7 +79,6 @@ let customerResponse03 = [
   `How do you not know what credits are?`
 ];
 
-
 let line1 = random(dialogue);
 let line2 = document.getElementById(`line-2`);
 let line1P = document.getElementById(`line-1`);
@@ -59,6 +88,19 @@ line1P.innerText = line1;
 function random(array) {
   let index = Math.floor(Math.random() * array.length);
   return array[index];
+}
+
+let price;
+let priceP = document.getElementById(`price`);
+priceP.innerText = price;
+if (line1 === `Give me 100$.`) {
+  priceP.innerText = `100$`
+}
+if (line1 === `Give me 200$.`) {
+  priceP.innerText = `200$`
+}
+if (line1 === `Give me 300$.`) {
+  priceP.innerText = `300$`
 }
 
 const kd = document.querySelectorAll(".key");
@@ -80,18 +122,79 @@ let keyPressed = (e) => {
       secretSound.play();
     }
     // Customer Response / Costomer & User Interaction
-    let result1 = text.includes(`what`);
+    let result1 = text.includes(`huh`);
     if ((kc === 13) && (result1 === true)) {
       line1P.innerText = random(customerResponse02);
     }
-    let result2 = text.includes(`credits`);
+    let result2 = text.includes(`what are credits`);
     if ((kc === 13) && (result2 === true)) {
       line1P.innerText = random(customerResponse03);
     }
-    let result3 = text.includes(`joke`);
+    let result3 = text.includes(`tell me a joke`);
     if ((kc === 13) && (result3 === true)) {
       line1P.innerText = random(customerJoke);
       jokeSound.play();
+    }
+    let result4 = text.includes(`what is this`);
+    if ((kc === 13) && (result4 === true)) {
+      console.log(itemImage.src)
+      if (itemImage.src.includes (`Item-01`)) {
+        line1P.innerText = `It's a Vermit Worm.`
+      }
+      if (itemImage.src.includes(`Item-02`)) {
+        line1P.innerText = `It's a Ugurt Flower.`
+      }
+      if (itemImage.src.includes(`Item-03`)) {
+        line1P.innerText = `It's a Gem Berry.`
+      }
+      if (itemImage.src.includes(`Item-04`)) {
+        line1P.innerText = `It's a Folmar in a Jar.`
+      }
+      if (itemImage.src.includes(`Item-05`)) {
+        line1P.innerText = `It's a Ceremonial Vase.`
+      }
+      if (itemImage.src.includes(`Item-06`)) {
+        line1P.innerText = `It's a Beskar Steel Ingot.`
+      }
+      if (itemImage.src.includes(`Item-07`)) {
+        line1P.innerText = `It's a Pound of Spice.`
+      }
+      if (itemImage.src.includes(`Item-08`)) {
+        line1P.innerText = `It's a Drogo Scale.`
+      }
+      if (itemImage.src.includes(`Item-09`)) {
+        line1P.innerText = `It's a Ghost Scripture.`
+      }
+      if (itemImage.src.includes(`Item-10`)) {
+        line1P.innerText = `It's a Unknown Organ.`
+      }
+      if (itemImage.src.includes(`Item-11`)) {
+        line1P.innerText = `It's a Uranium Rod.`
+      }
+      if (itemImage.src.includes(`Item-12`)) {
+        line1P.innerText = `It's a Laser Crystal.`
+      }
+    }
+    let result5 = text.includes(`stolen`);
+    if ((kc === 13) && (result5 === true)) {
+      let mandoStolen = clientImage.src.includes(`Mando`);
+      if ((itemImage.src.includes (`Item-06`)) && (mandoStolen === false)) {
+        line1P.innerText = `Yes`
+      }
+      else if ((itemImage.src.includes (`Item-06`)) && (mandoStolen === true)) {
+        line1P.innerText = `No`
+      }
+      let ghostStolen = clientImage.src.includes(`Ghost`);
+      if ((itemImage.src.includes (`Item-09`)) && (ghostStolen === false)) {
+        line1P.innerText = `Yes`
+      }
+      else if ((itemImage.src.includes (`Item-09`)) && (ghostStolen === true)) {
+        line1P.innerText = `No`
+      }
+      let organStolen = itemImage.src.includes(`Item-10`);
+      if ((itemImage.src.includes (`Item-10`)) && (ghostStolen === true)) {
+        line1P.innerText = `Yes`
+      }
     }
 }
 
